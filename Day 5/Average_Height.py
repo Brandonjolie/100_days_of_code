65fd2fe13190d2b2d4c60dfbaad20138c6e4c02d
Brandon Jolie <brandonjolie@gmail.com>
2022-05-26 12:23:30 -0400

    Day 5 Avg height

--- Day 5/Average_Height.py
+++ Day 5/Average_Height.py
@@ -1,6 +1,5 @@
 # 🚨 Don't change the code below 👇
-# student_heights = input("Input a list of student heights ").split()
-student_heights = '180 124 165 173 189 169 146'.split()
+student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
@@ -12,5 +11,4 @@ total = 0
 for val in student_heights:
     total += val
     count += 1
-print(total)
 print(round((total/count)))
