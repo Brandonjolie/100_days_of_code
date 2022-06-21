2aa666d7991bbd19fafcb2cfbb2ecb5f54fb368c
Brandon Jolie <brandonjolie@gmail.com>
2022-06-21 16:18:20 -0400

    Day 32

---
+++ Day 32/birthday_emails.py
@@ -0,0 +1,52 @@
+import pandas
+import datetime
+import random
+import smtplib
+import os
+
+
+def create_templates():
+    letters = []
+    letters.append(
+        open("/home/bjolie/100daysofpython/Day 32/letter_templates/letter_1.txt").read()
+    )
+    letters.append(
+        open("/home/bjolie/100daysofpython/Day 32/letter_templates/letter_3.txt").read()
+    )
+    letters.append(
+        open("/home/bjolie/100daysofpython/Day 32/letter_templates/letter_2.txt").read()
+    )
+    return letters
+
+
+def send_birthday_email(letters, name, sendto):
+    letter_template = random.choice(letters)
+    gmail_user = os.environ.get("gmail_user")
+    gmail_pass = os.environ.get("gmail_pass")
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(gmail_user, gmail_pass)
+    connection.sendmail(
+        from_addr=gmail_user,
+        to_addrs={sendto},
+        msg=f"Subject:Hello\n\n{letter_template}".replace("[NAME]", name),
+    )
+
+
+def main():
+    today = datetime.datetime.today().date()
+    birthday_data = pandas.read_csv("/home/bjolie/100daysofpython/Day 32/birthdays.csv")
+    dict_data = birthday_data.to_dict(orient="records")
+    for birthday in dict_data:
+        year = birthday["year"]
+        day = birthday["day"]
+        month = birthday["month"]
+        email = birthday["email"]
+        name = birthday["name"]
+        new_date = datetime.datetime(year, month, day).date()
+        letter_templates = create_templates()
+        if today == new_date:
+            send_birthday_email(letter_templates, name, email)
+
+
+main()
---
+++ Day 32/main.py
@@ -0,0 +1,26 @@
+import smtplib
+import os
+import datetime, random
+
+
+def send_motivational_email():
+    data = []
+    with open("/home/bjolie/100daysofpython/Day 32/quotes.txt") as quotes_file:
+        data = quotes_file.readlines()
+    quote = random.choice(data)
+    gmail_user = os.environ.get("gmail_user")
+    gmail_pass = os.environ.get("gmail_pass")
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(gmail_user, gmail_pass)
+    connection.sendmail(
+        from_addr=gmail_user,
+        to_addrs="bjolie@beanfield.com",
+        msg=f"Subject:Hello\n\n{quote}",
+    )
+    connection.close()
+
+
+current_day = datetime.datetime.today().weekday()
+if current_day == 1:
+    send_motivational_email()
